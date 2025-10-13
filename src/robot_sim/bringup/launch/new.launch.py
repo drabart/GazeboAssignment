43b64c7bebad9caa0c6dad17b1fe43f1c858ed2b
drabart <drabart@outlook.com>
2025-10-13 16:31:41 +0200

    updated launch script

--- src/robot_sim/bringup/launch/new.launch.py
+++ src/robot_sim/bringup/launch/new.launch.py
@@ -1,24 +1,9 @@
-# Copyright 2022 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -27,31 +12,35 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Configure ROS nodes for launch
-
     # Setup project paths
     pkg_project_bringup = get_package_share_directory('bringup')
     pkg_project_gazebo = get_package_share_directory('gazebo')
     pkg_project_description = get_package_share_directory('model_description')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
+    # --- Add model path so Gazebo can find base_plate ---
+    model_path = os.path.join(pkg_project_description, 'models')
+    os.environ['GZ_SIM_RESOURCE_PATH'] = (
+        os.environ.get('GZ_SIM_RESOURCE_PATH', '') + ':' + model_path
+    )
+
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'models', 'base_plate', 'robot.sdf')
+    sdf_file = os.path.join(pkg_project_description, 'models', 'base_plate', 'robot.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
-    # Setup to launch the simulator and Gazebo world
+    # Launch Gazebo with your custom world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'demo_world.sdf'
+            'new_world.sdf'
         ])}.items(),
     )
 
-    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
+    # Publish robot state
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -63,15 +52,15 @@ def generate_launch_description():
         ]
     )
 
-    # Visualize in RViz
-    # rviz = Node(
-    #    package='rviz2',
-    #    executable='rviz2',
-    #    arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'diff_drive.rviz')],
-    #    condition=IfCondition(LaunchConfiguration('rviz'))
-    # )
+    # RViz (disabled by default)
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'diff_drive.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
 
-    # Bridge ROS topics and Gazebo messages for establishing communication
+    # Bridge between ROS and Gazebo
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -84,8 +73,12 @@ def generate_launch_description():
 
     return LaunchDescription([
         gz_sim,
-        None,
+        DeclareLaunchArgument(
+            'rviz',
+            default_value='false',  # disabled by default
+            description='Open RViz.'
+        ),
         bridge,
         robot_state_publisher,
-        None
+        rviz,
     ])
