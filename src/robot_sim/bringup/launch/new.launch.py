25fea1cac8c9f6010b96f01e886a04d5f389ad0d
drabart <drabart@outlook.com>
2025-10-16 15:20:25 +0200

    working control of rotation

--- src/robot_sim/bringup/launch/new.launch.py
+++ src/robot_sim/bringup/launch/new.launch.py
@@ -22,37 +22,65 @@ def generate_launch_description():
         os.environ.get('GZ_SIM_RESOURCE_PATH', '') + ':' + model_path
     )
 
-    # Load the SDF or URDF file from "description" package
-    # sdf_file = os.path.join(pkg_project_description, 'models', 'Qbert', 'qbert.sdf')
-    # with open(sdf_file, 'r') as infp:
-    #     robot_desc = infp.read()
-
     urdf_file = os.path.join(pkg_project_description, 'models', 'Qbert', 'qbert.urdf')
     robot_desc = Command(['xacro ', urdf_file])
+
+    arguments = {'gz_args': PathJoinSubstitution([
+        pkg_project_gazebo,
+        'worlds',
+        'qbert_world.sdf'
+    ])}
 
     # Launch Gazebo with custom world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={
-            'gz_args': PathJoinSubstitution([
-                pkg_project_gazebo,
-                'worlds',
-                'qbert_world.sdf'
-            ])
-        }.items(),
+        launch_arguments=arguments.items(),
     )
 
     # Publish robot state and joint states
-    robot_state_publisher = Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[
-            {'use_sim_time': True},
-            {'robot_description': robot_desc},
-        ]
+        output="both",
+        parameters=[{'robot_description': robot_desc}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'qbert.rviz')],
+    )
+    static_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_tf_odom_base',
+        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base'],
+        output='screen'
+    )
+    joint_state_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+    )
+
+    ros2_control_path = os.path.join(pkg_project_description, 'config', 'qbert_controllers.yaml')
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="both",
+        parameters=[ros2_control_path]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["j2_position_controller", "--param-file", ros2_control_path],
     )
 
     static_tf = Node(
@@ -69,8 +97,16 @@ def generate_launch_description():
         name='joint_state_publisher',
     )
 
+    static_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_tf_world_base',
+        arguments=['0', '0', '0', '0', '0', '0', 'world', 'base'],
+        output='screen'
+    )
+
     # Spawn robot immediately
-    spawn_entity = Node(
+    gazebo_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
         arguments=[
@@ -83,7 +119,7 @@ def generate_launch_description():
 
 
     # Bridge between ROS and Gazebo
-    bridge = Node(
+    gazebo_bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         parameters=[{
@@ -93,57 +129,17 @@ def generate_launch_description():
         output='screen'
     )
 
-    static_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_tf_odom_base',
-        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base'],
-        output='screen'
-    )
-
-    joint_state = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"],
-    )
-
-    robot_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "j2_position_controller",
-            "--param-file",
-            os.path.join(pkg_project_description, 'config', 'qbert_controllers.yaml'),
-            "--controller-ros-args",
-            "-r /j2_position_controller/cmd_vel:=/cmd_vel",
-        ],
-    )
-
-    # RViz (disabled by default)
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'qbert.rviz')],
-        parameters=[{
-            "use_sim_time": True,
-        }],
-        condition=IfCondition(LaunchConfiguration('rviz'))
-    )
-
-    return LaunchDescription([
+    nodeList = [
         gz_sim,
-        bridge,
-        robot_state_publisher,
-        spawn_entity,
-        static_tf,
-        joint_state,
-        rviz,
+        gazebo_bridge_node,
+        gazebo_spawn_entity,
+        robot_state_publisher_node,
+        rviz_node,
+        control_node,
         joint_state_broadcaster_spawner,
         robot_controller_spawner,
-    ])
+        static_tf_node,
+        joint_state_node
+    ]
+
+    return LaunchDescription(nodeList)
